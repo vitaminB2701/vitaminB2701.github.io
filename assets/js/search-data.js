3983350aec8ad62a215f9c633f943dc88ffc85c5
vitaminB2701 <longtrumdaugau1997@gmail.com>
2026-01-25 06:28:33 +0000

    Deploying to gh-pages from @ vitaminB2701/vitaminB2701.github.io@04c86964399378e580e3b2a887d341c6692e27fe 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -511,14 +511,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%6C%6F%6E%67.%6E%67%75%79%65%6E@%6E%74%75.%65%64%75.%73%67", "_blank");
         },
       },{
         id: 'social-rss',
@@ -532,14 +525,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=Z6aVtJgAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
