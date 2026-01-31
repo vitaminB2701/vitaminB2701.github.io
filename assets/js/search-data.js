ef9bfc1c918f4efeb1afc59d904907f739889f41
vitaminB2701 <longtrumdaugau1997@gmail.com>
2026-01-31 15:41:37 +0000

    Deploying to gh-pages from @ vitaminB2701/vitaminB2701.github.io@4a546ce9e987049e3fc4922dd889d965341530f6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,9 +16,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+        },{id: "nav-profile",
+          title: "Profile",
+          description: "For my academic CV, click the button on the right.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
