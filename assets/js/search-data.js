a39b2f63c99457023eab75e29db81aa8036e5c80
vitaminB2701 <longtrumdaugau1997@gmail.com>
2026-02-04 12:26:31 +0000

    Deploying to gh-pages from @ vitaminB2701/vitaminB2701.github.io@e8cc4e3c15150866a602a9d8737c7b704641ddd9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -495,7 +495,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/academicCV.pdf", "_blank");
         },
       },{
       id: 'light-theme',
